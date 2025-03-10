11c4872d938ab075a26b27f1a681401f767d9d32
mi6gun <mi6gun@mail.com>
2025-03-10 21:13:48 +0500

    Файл куда вы должны засунуть свой api ключ

---
+++ NihaoTyanBot/DreamGirl/Config.cs
@@ -0,0 +1,16 @@
+
+namespace NihaoTyan.Bot
+{
+    static class Config
+    {
+        public static readonly string Token;
+        public static readonly string geminiKey;
+
+        static Config()
+        {
+            Token = "Здесь_могла_быть_ваша_реклама";
+            geminiKey = "А_б_в_г_д_ейка";
+
+        }
+    }
+}
