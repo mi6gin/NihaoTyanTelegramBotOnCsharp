a067e41379fe662e256cd963c453c923d52cb7fb
mi6gun <mi6gun@mail.com>
2025-03-10 21:09:04 +0500

    Файл назначения команд, чтобы пользователь мог их все увидеть с небольшим описанием

---
+++ NihaoTyanBot/DreamGirl/commandsList/Listofcommands.cs
@@ -0,0 +1,37 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace NihaoTyan.Bot.commandsList
+{
+    public class Listofcommands
+    {
+        private static bool _commandsRegistered = false;
+
+        public static async Task AllCommands(Message message, ITelegramBotClient botClient)
+        {
+            try
+            {
+                // Регистрация команд в Telegram только если они еще не зарегистрированы
+                if (!_commandsRegistered)
+                {
+                    var commands = new List<BotCommand>
+                    {
+                        new BotCommand { Command = "start", Description = "Поздороватся с Нихао" },
+                        new BotCommand { Command = "help", Description = "Позвать Нихао" },
+                        new BotCommand { Command = "dedinside", Description = "Позвать собутылника" },
+                        new BotCommand { Command = "stf", Description = "Режим прогулки" },
+                        new BotCommand { Command = "gpt", Description = "Спроси Нихао" },
+                        new BotCommand { Command = "anecdote", Description = "Расскажи анекдот про Штирлица" }
+                    };
+
+                    await botClient.SetMyCommandsAsync(commands);
+                    _commandsRegistered = true; // Устанавливаем флаг, что команды зарегистрированы
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
