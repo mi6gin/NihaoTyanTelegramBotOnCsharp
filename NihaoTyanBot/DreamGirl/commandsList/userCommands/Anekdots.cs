ebbe5fcfa4d141a3e7d87ef032e875295a1fd995
mi6gun <mi6gun@mail.com>
2025-03-10 21:05:20 +0500

    Добавление команды /anecdote, анекдоты про штирлица

---
+++ NihaoTyanBot/DreamGirl/commandsList/userCommands/Anekdots.cs
@@ -0,0 +1,42 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace NihaoTyan.Bot.commandsList.userCommands
+{
+    public static class Anecdote
+    {
+        private static readonly Random _random = new Random(); // Используем один экземпляр Random для избежания повторений
+        private static readonly string FilePath = Path.Combine(
+            "DreamGirl", "commandsList", "userCommands", "mediaFiles", "anekdots.txt");
+
+
+
+        public static async Task SendAsync(Message message, ITelegramBotClient botClient)
+        {
+            if (!System.IO.File.Exists(FilePath))
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Вся жизнь твоя – шутка \ud83d\ude06");
+                return;
+            }
+
+            var content = await System.IO.File.ReadAllTextAsync(FilePath);
+            var anecdotes = content.Split(new[] { "\r\n\r\n", "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (anecdotes.Length == 0)
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "В файле нет анекдотов.");
+                return;
+            }
+
+            string randomAnecdote = anecdotes[_random.Next(anecdotes.Length)];
+
+            // Telegram ограничивает длину сообщения 4096 символами
+            if (randomAnecdote.Length > 4096)
+            {
+                randomAnecdote = randomAnecdote.Substring(0, 4093) + "...";
+            }
+
+            await botClient.SendTextMessageAsync(message.Chat.Id, randomAnecdote);
+        }
+    }
+}
