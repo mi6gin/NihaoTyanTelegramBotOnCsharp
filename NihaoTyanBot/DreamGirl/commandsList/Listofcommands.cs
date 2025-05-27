e40699044fcc5377435395d835b33065f0b47f21
mi6gun <80161464+mi6gin@users.noreply.github.com>
2025-05-27 13:24:14 +0500

    Update Listofcommands.cs

--- NihaoTyanBot/DreamGirl/commandsList/Listofcommands.cs
+++ NihaoTyanBot/DreamGirl/commandsList/Listofcommands.cs
@@ -21,6 +21,7 @@ namespace NihaoTyan.Bot.commandsList
                         new BotCommand { Command = "dedinside", Description = "Позвать собутылника" },
                         new BotCommand { Command = "stf", Description = "Режим прогулки" },
                         new BotCommand { Command = "gpt", Description = "Спроси Нихао" },
+                        new BotCommand { Command = "canel", Description = "Завершить разговор" },
                         new BotCommand { Command = "anecdote", Description = "Расскажи анекдот про Штирлица" }
                     };
 
